c3e78663ee7d28903ba6fb258d048ee23b0c9bfb
mohanagarwal0910 <mohanagarwal09101@gmail.com>
2025-01-08 12:24:58 +0530

    change in code

--- conversion.java
+++ conversion.java
@@ -4,7 +4,7 @@ class conversion
     public static void main(String []args)
     {
         Scanner sc=new Scanner(System.in);
-        double a=sc.nextInt();
+        double celsius=sc.nextInt();
         double fahrenheit = (celsius * 9/5) + 32;
         System.out.println(fahrenheit);
     }
